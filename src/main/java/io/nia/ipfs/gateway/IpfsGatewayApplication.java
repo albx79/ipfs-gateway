19a4327dad530bcef8aa03d14f702c0f369ade38
Team Hermes <hermes@sky.com>
2017-05-02 11:12:19 +0100

    initial commit

---
+++ src/main/java/io/nia/ipfs/gateway/IpfsGatewayApplication.java
@@ -0,0 +1,12 @@
+package io.nia.ipfs.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpfsGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpfsGatewayApplication.class, args);
+	}
+}
---
+++ src/test/java/io/nia/ipfs/gateway/IpfsGatewayApplicationTests.java
@@ -0,0 +1,16 @@
+package io.nia.ipfs.gateway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IpfsGatewayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
